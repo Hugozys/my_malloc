a398e3422bfc1a37edf0b358b0c2cbb2c2557840
hugo <yz395@duke.edu>
2018-01-30 19:40:41 -0500

    finished

---
+++ 002_malloc_thread_safe/thread_tests/run_all.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo " *******************Result of Lock Free*****************" >> lock_free.txt
+
+echo"">>lock.txt
+echo"">>lock.txt
+
+
+for i in thread_test_malloc_free thread_test_measurement thread_test_malloc_free_change_thread thread_test
+do
+    echo "========================${i}===========================" >> lock.txt
+    (time ./$i) >> lock.txt 2>&1
+    echo "" >> lock.txt
+    echo "" >> lock.txt
+done
